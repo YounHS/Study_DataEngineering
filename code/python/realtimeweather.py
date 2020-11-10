51cfd5605504e8e32fba75ec37c8afea31ab511b
hightophades@naver.com <hightophades@naver.com>
2020-11-10 10:03:07 +0900

    naver weather html tag constructure change, so we change code too

--- code/python/realtimeweather.py
+++ code/python/realtimeweather.py
@@ -14,42 +14,50 @@ while 1:
 
     currentTemperature = soup.find('strong', {'class': 'current'}).text.replace("현재 온도", '')
     currentTemperature = currentTemperature.replace('°', '')
-    print(currentTemperature)
+    print('현재온도: ', currentTemperature)
 
-    highTemperature = soup.find('strong', {'class': 'degree_height'}).text.replace("최고온도", '')
-    highTemperature = highTemperature.replace('°', '')
-    print(highTemperature)
+    temperature = soup.find('strong', {'class': 'temperature'}).text.replace("최저기온", '')
+    temperature = temperature.replace("최고기온", '')
+    temperature = temperature.replace("/", '')
+    highTemperature = temperature.split('°')[0]
+    lowTemperature = temperature.split('°')[1]
+    # highTemperature = highTemperature.replace('°', '')
+    print('최저기온: ', highTemperature)
 
-    lowTemperature = soup.find('strong', {'class': 'degree_low'}).text.replace("최저온도", '')
-    lowTemperature = lowTemperature.replace('°', '')
-    print(lowTemperature)
+    # lowTemperature = soup.find('strong', {'class': 'temperature'}).find('span', {'class': 'blind'})[1].text.replace("최고기온", '')
+    # lowTemperature = temperature.findAll('span')[2].text.replace("최고기온", '')
+    # lowTemperature = lowTemperature.replace('°', '')
+    print('최고기온: ', lowTemperature)
 
-    feelTemperature = soup.find('strong', {'class': 'degree_feel'}).text.replace('°', '')
-    print(feelTemperature)
+    feelTemperature = soup.find('dd', {'class': 'desc_feeling'}).text.replace('°', '')
+    print('체감기온: ', feelTemperature)
 
-    timeRainFall = soup.find('a', {'class': 'link_rainfall'}).text.replace("시간당 강수량 ", '')
-    timeRainFall = timeRainFall.replace("mm", '')
-    print(timeRainFall)
+    rainFall = soup.find('dd', {'class': 'desc_rainfall'}).text.replace("%", '')
+    print('강수확률: ', rainFall)
+
+    wind = soup.find('dd', {'class': 'desc_wind'}).text
+    print('바람: ', wind)
+
+    humidity = soup.find('dd', {'class': 'desc_humidity'}).text.replace('%','')
+    print('습도: ', humidity)
 
     liFineDust = soup.find('ul', {'class': 'today_chart_list'})
     fineDust = liFineDust.findAll('li')[0]
     fineDust = fineDust.find('strong', {'class': 'value'}).text
-    print(fineDust)
+    print('미세먼지: ', fineDust)
 
     liUltraFineDust = soup.find('ul', {'class': 'today_chart_list'})
     ultraFineDust = liUltraFineDust.findAll('li')[1]
     ultraFineDust = ultraFineDust.find('strong', {'class': 'value'}).text
-    print(ultraFineDust)
-    # ultraFineDust = liUltraFineDust[1].find('strong', {'class': 'value'}).text
-    # print(ultraFineDust)
+    print('초미세먼지: ', ultraFineDust)
 
     liOzone = soup.find('ul', {'class': 'today_chart_list'})
     ozone = liOzone.findAll('li')[2]
     ozone = ozone.find('strong', {'class': 'value'}).text
-    print(ozone)
+    print('자외선', ozone)
 
     update = soup.find('span', {'class': 'title_dsc'}).text.replace(" 업데이트", '')
-    print(update)
+    print('최근 업데이트 시각: ', update)
 
 
 # conn = psycopg2.connect(conn_string)
@@ -61,7 +69,9 @@ while 1:
 #             "high_temperature TEXT, "
 #             "low_temperature TEXT, "
 #             "feel_temperature TEXT, "
-#             "time_rain_fall TEXT, "
+#             "rain_fall TEXT, "
+#             "windms TEXT, "
+#             "humidities TEXT, "
 #             "fine_dust TEXT, "
 #             "ultra_fine_dust TEXT, "
 #             "ozone TEXT, "
@@ -76,15 +86,18 @@ while 1:
                 "high_temperature, "
                 "low_temperature, "
                 "feel_temperature, "
-                "time_rain_fall, "
+                "rain_fall, "
+                "windms, "
+                "humidities, "
                 "fine_dust, "
                 "ultra_fine_dust, "
                 "ozone, "
                 "server_update_time) "
                 "VALUES (nextval('realtime_env_naver_seq_seq'), %s, %s, %s, %s, %s, %s, %s, %s, %s)", (
                     currentTemperature, highTemperature, lowTemperature, feelTemperature,
-                    timeRainFall, fineDust, ultraFineDust, ozone, update))
+                    rainFall, wind, humidity, fineDust, ultraFineDust, ozone, update))
     conn.commit()
     cur.close()
     conn.close()
     time.sleep(60)
+
