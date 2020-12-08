265f93b1356456003c4872a3e2a281bc48928f2b
YounHS <hightophades@naver.com>
2020-12-09 00:15:09 +0900

    worldometer corona data pipeline processing

--- code/python/Corona/selenium_corona_crawling.py
+++ code/python/Corona/selenium_corona_crawling.py
@@ -2,10 +2,43 @@ __author__ = 'Hyeonsoo Youn'
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from kafka import KafkaProducer
+
 import psycopg2
 import time
+import json
+import datetime
 
 conn_string = "host='localhost' dbname ='collectData' user='postgres' password='secy'"
+
+
+def kafka_pub(wo_saveUpdated, wo_CoronavirusCase, wo_DeathNum, wo_RecoveredNum, wo_ActiveCase,
+              wo_ActiveMildCondition, wo_ActiveSeriousCritical, wo_ClosedCase, wo_ClosedRecoverdDischarged,
+              wo_ClosedDeath):
+    now = datetime.datetime.now()
+    nowDateTime = now.strftime('%Y-%m-%d %H:%M:%S')
+    producer = KafkaProducer(bootstrap_servers=["211.194.13.200:9092"])
+    topicName = "corona"
+    msg = {"wo_saveUpdated": wo_saveUpdated, "wo_CoronavirusCase": int(wo_CoronavirusCase), "wo_DeathNum": int(wo_DeathNum),
+           "wo_RecoveredNum": int(wo_RecoveredNum), "wo_ActiveCase": int(wo_ActiveCase),
+           "wo_ActiveMildCondition": int(wo_ActiveMildCondition), "wo_ActiveSeriousCritical": int(wo_ActiveSeriousCritical),
+           "wo_ClosedCase": int(wo_ClosedCase), "wo_ClosedRecoverdDischarged": int(wo_ClosedRecoverdDischarged),
+           "wo_ClosedDeath": int(wo_ClosedDeath), "insertTime": nowDateTime}
+
+    def on_send_success(record_metadata):
+        print(record_metadata.topic)
+        print(record_metadata.partition)
+        print(record_metadata.offset)
+
+    # def on_send_error(excp):
+    #     log.error("error!!!", exc_info=excp)
+
+    producer = KafkaProducer(value_serializer=lambda m: json.dumps(msg).encode("ascii"))
+    producer.send(topicName, {'key': 'value'}).add_callback(on_send_success)
+
+    producer.flush()
+
+    producer = KafkaProducer(retries=5)
 
 
 def createDB():
@@ -60,7 +93,7 @@ def main():
     driver = webdriver.Chrome('C:\chromedriver.exe', chrome_options=chrome_options)
 
     count = True
-    saveUpdated = ''
+    wo_saveUpdated = ''
 
     while 1:
         # driver.implicitly_wait(5)
@@ -71,84 +104,90 @@ def main():
         nowUpdated = driver.find_element_by_xpath("//div[normalize-space(@class)='content-inner']//div[2]").text
 
         if count or len(saveUpdated) == 0:
-            worldCoronavirusCase = driver.find_element_by_xpath(
+            wo_CoronavirusCase = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[4]/div[1]/span[1]").text.replace(',', '')
-            worldDeathNum = driver.find_element_by_xpath(
+            wo_DeathNum = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[6]/div[1]/span[1]").text.replace(',', '')
-            worldRecoveredNum = driver.find_element_by_xpath(
+            wo_RecoveredNum = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[7]/div[1]/span[1]").text.replace(',', '')
-            worldActiveCase = driver.find_element_by_xpath(
+            wo_ActiveCase = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[9]/div[1]/div[2]/div[1]/div[1]/div[1]").text.replace(
                 ',', '')
-            worldActiveMildCondition = driver.find_element_by_xpath(
+            wo_ActiveMildCondition = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[9]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/span[1]").text.replace(
                 ',', '')
-            worldActiveSeriousCritical = driver.find_element_by_xpath(
+            wo_ActiveSeriousCritical = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[9]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/span[1]").text.replace(
                 ',', '')
-            worldClosedCase = driver.find_element_by_xpath(
+            wo_ClosedCase = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[10]/div[1]/div[2]/div[1]/div[1]/div[1]").text.replace(
                 ',', '')
-            worldClosedRecoverdDischarged = driver.find_element_by_xpath(
+            wo_ClosedRecoverdDischarged = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[10]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/span[1]").text.replace(
                 ',', '')
-            worldClosedDeath = driver.find_element_by_xpath(
+            wo_ClosedDeath = driver.find_element_by_xpath(
                 "//div[normalize-space(@class)='content-inner']//div[10]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/span[1]").text.replace(
                 ',', '')
 
-            saveUpdated = nowUpdated
-            print(saveUpdated)
+            wo_saveUpdated = nowUpdated
+            print(wo_saveUpdated)
             print('\n\n전세계 상황')
-            print(worldCoronavirusCase)
-            print(worldDeathNum)
-            print(worldRecoveredNum)
-            print(worldActiveCase)
-            print(worldActiveMildCondition)
-            print(worldActiveSeriousCritical)
-            print(worldClosedCase)
-            print(worldClosedRecoverdDischarged)
-            print(worldClosedDeath)
+            print(wo_CoronavirusCase)
+            print(wo_DeathNum)
+            print(wo_RecoveredNum)
+            print(wo_ActiveCase)
+            print(wo_ActiveMildCondition)
+            print(wo_ActiveSeriousCritical)
+            print(wo_ClosedCase)
+            print(wo_ClosedRecoverdDischarged)
+            print(wo_ClosedDeath)
+            kafka_pub(wo_saveUpdated, wo_CoronavirusCase, wo_DeathNum, wo_RecoveredNum, wo_ActiveCase,
+                      wo_ActiveMildCondition, wo_ActiveSeriousCritical, wo_ClosedCase, wo_ClosedRecoverdDischarged,
+                      wo_ClosedDeath)
             count = False
         else:
             # 여기서부터 새롭게 업데이트된 데이터 받아오기
-            if (nowUpdated != '-') and (saveUpdated != nowUpdated):
-                worldCoronavirusCase = driver.find_element_by_xpath(
+            if (nowUpdated != '-') and (wo_saveUpdated != nowUpdated):
+                wo_CoronavirusCase = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[4]/div[1]/span[1]").text.replace(',', '')
-                worldDeathNum = driver.find_element_by_xpath(
+                wo_DeathNum = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[6]/div[1]/span[1]").text.replace(',', '')
-                worldRecoveredNum = driver.find_element_by_xpath(
+                wo_RecoveredNum = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[7]/div[1]/span[1]").text.replace(',', '')
-                worldActiveCase = driver.find_element_by_xpath(
+                wo_ActiveCase = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[9]/div[1]/div[2]/div[1]/div[1]/div[1]").text.replace(
                     ',', '')
-                worldActiveMildCondition = driver.find_element_by_xpath(
+                wo_ActiveMildCondition = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[9]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/span[1]").text.replace(
                     ',', '')
-                worldActiveSeriousCritical = driver.find_element_by_xpath(
+                wo_ActiveSeriousCritical = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[9]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/span[1]").text.replace(
                     ',', '')
-                worldClosedCase = driver.find_element_by_xpath(
+                wo_ClosedCase = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[10]/div[1]/div[2]/div[1]/div[1]/div[1]").text.replace(
                     ',', '')
-                worldClosedRecoverdDischarged = driver.find_element_by_xpath(
+                wo_ClosedRecoverdDischarged = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[10]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/span[1]").text.replace(
                     ',', '')
-                worldClosedDeath = driver.find_element_by_xpath(
+                wo_ClosedDeath = driver.find_element_by_xpath(
                     "//div[normalize-space(@class)='content-inner']//div[10]/div[1]/div[2]/div[1]/div[1]/div[3]/div[2]/span[1]").text.replace(
                     ',', '')
 
-                saveUpdated = nowUpdated
-                print(saveUpdated)
+                wo_saveUpdated = nowUpdated
+                print(wo_saveUpdated)
                 print('\n\n전세계 상황')
-                print(worldCoronavirusCase)
-                print(worldDeathNum)
-                print(worldRecoveredNum)
-                print(worldActiveCase)
-                print(worldActiveMildCondition)
-                print(worldActiveSeriousCritical)
-                print(worldClosedCase)
-                print(worldClosedRecoverdDischarged)
-                print(worldClosedDeath)
+                print(wo_CoronavirusCase)
+                print(wo_DeathNum)
+                print(wo_RecoveredNum)
+                print(wo_ActiveCase)
+                print(wo_ActiveMildCondition)
+                print(wo_ActiveSeriousCritical)
+                print(wo_ClosedCase)
+                print(wo_ClosedRecoverdDischarged)
+                print(wo_ClosedDeath)
+                kafka_pub(wo_saveUpdated, wo_CoronavirusCase, wo_DeathNum, wo_RecoveredNum, wo_ActiveCase,
+                          wo_ActiveMildCondition, wo_ActiveSeriousCritical, wo_ClosedCase, wo_ClosedRecoverdDischarged,
+                          wo_ClosedDeath)
 
 
 if __name__ == "__main__":
